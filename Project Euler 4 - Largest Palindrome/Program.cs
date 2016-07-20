555a4c99d609e314351596c819be3844b3e99dd8
Josh <Josh@JOSH-DESKTOP>
2016-07-20 18:27:06 +0100

    Add project files.

---
+++ Project Euler 4 - Largest Palindrome/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler_4___Largest_Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
